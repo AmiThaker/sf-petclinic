73c88be9dc5518eeb222c0d69cb3d504470a7bd3
AmiThaker <thakerami139@gmail.com>
2024-02-19 16:20:10 +0530

    Refactoring to Common Interface. Closes #11

--- petclinic-data/src/main/java/com/example/sfpetclinic/service/VetService.java
+++ petclinic-data/src/main/java/com/example/sfpetclinic/service/VetService.java
@@ -4,10 +4,4 @@ import com.example.sfpetclinic.model.Vet;
 
 import java.util.Set;
 
-public interface VetService {
-
-    Vet findById(Long id);
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
-}
+public interface VetService extends CrudService<Vet,Long>{}
